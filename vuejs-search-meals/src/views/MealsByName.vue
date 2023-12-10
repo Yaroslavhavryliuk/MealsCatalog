ed5ecf6020d8ecb46884d6cd271d83c9fc559395
Yaroslavhavryliuk <gv_yarik@ukr.net>
2023-12-10 19:26:29 +0200

    All is done
    Search by Ingredient done and styles updates

--- vuejs-search-meals/src/views/MealsByName.vue
+++ vuejs-search-meals/src/views/MealsByName.vue
@@ -1,17 +1,18 @@
 <template>
-    <div class="p-8 pb-0">
-        <input
-            type="text"
-            v-model="keyword"
-            class="rounded border-2 border-gray-200 w-full"
-            placeholder="Search for Meals"
-            @change="searchMeals"
-        />
-    </div>
+  <div class="p-8 pb-0">
+    <h1 class="text-4xl font-bold mb-4 text-orange-500">Search Meals by Name</h1>
+  </div>
+  <div class="px-8 pb-3">
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded border-2 bg-white border-gray-200 focus:ring-orange-500 focus:border-orange-500 w-full"
+      placeholder="Search for Meals"
+      @change="searchMeals"
+    />
+  </div>
 
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-       <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
-    </div>
+  <Meals :meals="meals" />
 </template>
 
 <script setup>
@@ -19,21 +20,24 @@ import { computed } from "@vue/reactivity";
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import store from "../store";
-import YouTubeButton from "../components/YouTubeButton.vue"
-import MealItem from "../components/MealItem.vue";
+import Meals from "../components/Meals.vue";
 
 const route = useRoute();
 const keyword = ref("");
 const meals = computed(() => store.state.searchedMeals);
 
 function searchMeals() {
-    store.dispatch('searchMeals', keyword.value)
+  if (keyword.value) {
+    store.dispatch("searchMeals", keyword.value);
+  } else {
+    store.commit("setSearchedMeals", []);
+  }
 }
 
 onMounted(() => {
-    keyword.value = route.params.name
-    if (keyword.value) {
-        searchMeals()
-    }
-})
+  keyword.value = route.params.name;
+  if (keyword.value) {
+    searchMeals();
+  }
+});
 </script>
